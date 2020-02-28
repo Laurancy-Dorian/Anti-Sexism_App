7f9fb03b3312d4b310ab7ff85efb880697bcfebe
Polytech <user164247@92-63-141-248.rdns.melbourne.co.uk>
2020-02-28 15:14:07 +0000

    Project created

---
+++ Swift_App/Anti-Feminism_App/Anti-Feminism_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Anti-Feminism_App
+//
+//  Created by user164247 on 2/28/20.
+//  Copyright © 2020 Anthony_Dorian_Emma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift_App/Anti-Feminism_App/Anti-Feminism_AppTests/Anti_Feminism_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Anti_Feminism_AppTests.swift
+//  Anti-Feminism_AppTests
+//
+//  Created by user164247 on 2/28/20.
+//  Copyright © 2020 Anthony_Dorian_Emma. All rights reserved.
+//
+
+import XCTest
+@testable import Anti_Feminism_App
+
+class Anti_Feminism_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
