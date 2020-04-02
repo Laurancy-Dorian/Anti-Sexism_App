696ced72bb84e7c3b242e92dfc1ae008cee3da8d
emma_cbrt <44927969+emma-cbrt@users.noreply.github.com>
2020-04-02 18:06:27 +0200

    Revert "correction"
    This reverts commit fc0d90af2146ef646d842b41df506c6b17260870.

--- frontend/antisexism/src/components/Header.js
+++ frontend/antisexism/src/components/Header.js
@@ -79,7 +79,7 @@ class Header extends Component {
     }
     return(
       <div className="header-app">
-          <Navbar className="d-flex justify-content-between" sticky="top">
+          <Navbar onSelect = {this.handleSelect} className="d-flex justify-content-between" sticky="top">
               <Nav>
                 <Link to="/">
                   <Navbar.Brand >
